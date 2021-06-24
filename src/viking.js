358eef3d192a7ff00d3cc6704d42bcd379b4fbbc
Senimtra <senimtra@hotmail.com>
2021-06-24 21:29:29 +0200

    solve iteration 2: viking

--- src/viking.js
+++ src/viking.js
@@ -14,8 +14,27 @@ class Soldier {
   }
 }
 
-// Viking
-class Viking {}
+// #########################
+// ## Iteration 2: Viking ##
+// #########################
+
+class Viking extends Soldier {
+  constructor(name, health, strength) {
+    super(health, strength);
+    this.name = name;
+  }
+  receiveDamage(damage) {
+    this.health -= damage;
+    if (this.health > 0) {
+      return `${this.name} has received ${damage} points of damage`;
+    } else {
+      return `${this.name} has died in act of combat`;
+    }
+  }
+  battleCry() {
+    return 'Odin Owns You All!';
+  }
+}
 
 // Saxon
 class Saxon {}
